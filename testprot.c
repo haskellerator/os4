32322c2cd5bfffe6b79276e60e529f1292a0d4f6
haskellerator <talzelig@gmail.com>
2014-06-12 21:41:05 +0300

    updated testprot

--- testprot.c
+++ testprot.c
@@ -1,6 +1,25 @@
 #include "types.h"
 #include "stat.h"
 #include "user.h"
+#include "fcntl.h"
+
+#define BUF_SIZE 128
+
+char buf2[512];
+
+void
+cat(int fd)
+{
+  int n;
+
+  while((n = read(fd, buf2, sizeof(buf2))) > 0)
+    write(1, buf2, n);
+  if(n < 0){
+    printf(1, "cat: read error\n");
+    exit();
+  }
+}
+
 
 int
 main(int argc, char *argv[])
@@ -11,18 +30,78 @@ main(int argc, char *argv[])
 
 	char * pathname = argv[1];
 	char * password = argv[2];
+	char buf[BUF_SIZE];
+	int pid,unlock, fd, prot,br,unprot;
+	
+	// locks file with password
+	if((prot = fprot(pathname, password)) < 0){
+		printf(1,"protection failed: %d\n" ,prot);
+		exit();
+	} 
 
-	int prot = fprot(pathname, password); // locks file with password
-	printf(1,"prot result: %d\n" ,prot); 
-	int pid;
+	printf(1,"protection successful\n"); 
 
 	if((pid = fork()) == 0){ // child process
+		
+		// unlocks file
+		if((unlock = funlock(pathname,password)) < 0){
+			printf(1,"child: unlock failed: %d\n" ,unlock); 
+			exit();
+		} 
 
+		printf(1,"child: unlock successful\n"); 
+		
+		// open file to read
+		if((fd = open(pathname, O_RDONLY)) < 0){
+			printf(1,"child: open failed\n"); 
+			exit(); 		
+		}	
+
+		printf(1,"child: open successful\nchild: file contents are:\n"); 
+
+		// reads contents.
+		while((br = read(fd, buf, BUF_SIZE)) > 0){
+			printf(1,"%s",buf); // prints to screen
+		}	
+			
+		printf(1,"\nchild: finished reading\n");
+		
+		// close file
+		if(close(fd) < 0){
+			printf(1,"child: close failed\n"); 
+			exit(); 		
+		}	
+
+		printf(1,"\nchild: closed file\n");
+		exit();
+		
+	} else { // parent process
+		wait(); // wait for child
+
+		// open file
+		if((fd = open(pathname, O_RDONLY)) < 0){
+			printf(1,"parent: open failed\n");  // if failed, print failed to open
+			exit(); 		
+		}	
+		printf(1,"parent: open successful\nparent: now cat supposed to fail\n"); 
+
+
+
+		// read using cat while still protected
+		cat(fd); // supposed to fail
+
+		// unprotect
+		if((unprot = funprot(pathname,password)) < 0){
+			printf(1,"parent: unprot failed: %d\n" ,unprot); 
+			exit();
+		} 
+
+		printf(1,"parent: unprotect successful\nparent: now cat supposed to succeed"); 
+
+		cat(fd); // supposed to succeed
 	}
-   
-	int unprot = funprot("tal","zelig");    
-	int unlock = funlock("bulbul","akabulbul");
-	printf(1,"prot %d, unprot %d, unlock %d\n",prot,unprot,unlock);
+	
+	printf(1,"Test ended"); 
 	exit();
 }
 
